69ea133a584aa953e30f4679a6e7375a4565c2b2
Enrique Hernandez <enrique.hz.t@gmail.com>
2020-11-08 13:47:07 -0600

    Ready to submit

--- src/utils/filterPokemon.test.js
+++ src/utils/filterPokemon.test.js
@@ -4,17 +4,23 @@ import { mockPokemonsData } from "../mock/pokeData";
 describe("Filter Pokemon", () => {
   test("it should return an array", () => {
     /* Your solution comes here */
-    expect(false).toBe(true);
+
+    expect(Array.isArray(filterPokemon(mockPokemonsData))).toBe(true);
   });
 
   test("it should return an empty array", () => {
     /* Your solution comes here */
-    expect(false).toBe(true);
+    //filterPokemon();
+    expect(filterPokemon(mockPokemonsData, "").length).toStrictEqual(0);
   });
 
   test("it should return an array with charizard object", () => {
     /* Your solution comes here */
-    expect(false).toBe(true);
+    //filterPokemon(mockPokemonsData, "charizard");
+    //expect(false).toBe(true);
+    expect(
+      filterPokemon(mockPokemonsData, "charizard")[0].name === "charizard"
+    ).toBe(true);
   });
 
   /* BUNUS */
